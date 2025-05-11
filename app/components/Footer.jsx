54ddadd91b0834c72bd5991b93d23703497eeaaf
ZashShadow <143568466+ZashShadow@users.noreply.github.com>
2025-05-11 19:37:21 +0500

    oops should open in newtab

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer fixed bottom-2   max-tab:static   flex flex-col items-center">
-        <span className='text-[#D9B46E]'>Thanx for using my App 💗 This is my very first full-fledged project for web-dev, you can find me on <a className='text-white hover:text-white/70 underline' href="https://github.com/ZashShadow">GitHub</a>👋</span>
+        <span className='text-[#D9B46E]'>Thanx for using my App 💗 This is my very first full-fledged project for web-dev, you can find me on <a className='text-white hover:text-white/70 underline' href="https://github.com/ZashShadow" target="_blank">GitHub</a>👋</span>
     </div>
   )
 }
